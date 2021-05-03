f5b5d48f6546659ae1cff06bd31a2457093f469c
mmmommm <ryota1839@gmail.com>
2021-05-03 21:16:07 +0900

    delete: unnecessary code

--- connect_slack.ts
+++ connect_slack.ts
@@ -1,28 +1,3 @@
-export class Slackbot {
-  token: string;
-
-  constructor(token: string) {
-    this.token = token;
-  }
-
-  async sendMessage(channel: string, text: string) {
-    const response = await fetch("https://slack.com/api/chat.postMessage", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        "Authorization": `Bearer ${this.token}`,
-      },
-      body: JSON.stringify({
-        channel,
-        text,
-        as_user: true,
-      }),
-    });
-
-    return response;
-  }
-}
-
 export const sendMessage = async(token: string, channel: string, text: string) => {
   const response = await fetch("https://slack.com/api/chat.postMessage", {
     method: "POST",
